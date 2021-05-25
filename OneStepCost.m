b1c58575f78f07f708159b4b66136d9fd858f2ec
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-05-25 16:45:09 -0500

    One Step Cost function matlab

---
+++ OneStepCost.m
@@ -0,0 +1,34 @@
+function [cost]= OneStepCost(H,G,F,m,Q)
+% Given H,G, cost is the one step cost from H to G
+% if the cost is not a real value or negative, cost=0mea
+g=length(H);
+E=zeros(g,g);
+
+
+M=m*Q;
+q=F.*H/(F*H');
+
+for j=1:g
+    
+    for k=1:g
+        E(j,k)=exp(G(k)/(F(k)*H(k))-G(j)/(F(j)*H(j)));
+    end
+end
+
+
+sum2=0;
+sum1=sum(G.*log(G./q));
+
+
+for j=1:g
+    
+    for k=1:g
+        sum2=sum2+M(j,k)*F(j)*H(j)*(1-E(j,k));
+    end
+end                        
+cost=sum1+sum2;
+if isinf(cost)|| isnan(cost) || ~isreal(cost) || cost<0
+    cost=0;
+end
+
+end
