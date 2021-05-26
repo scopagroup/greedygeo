04a68a81c56cbf3765c8d8b150d71257cb93a1c1
Andreas Mang <andreas@math.uh.edu>
2021-05-26 10:04:20 -0500

    parallel execution added

---
+++ ml/execGeo.m
@@ -0,0 +1,16 @@
+p = gcp();
+
+
+% to request multiple evaluations, use a loop
+for idx = 1:10
+  f(idx) = parfeval(p,@magic,1,idx); % Square size determined by idx
+end
+
+% collect the results as they become available.
+magicResults = cell(1,10);
+for idx = 1:10
+  % fetchNext blocks until next results are available.
+  [completedIdx,value] = fetchNext(f);
+  magicResults{completedIdx} = value;
+  fprintf('Got result with index: %d.\n', completedIdx);
+end
