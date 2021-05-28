efc3b59af289c03b86cb6961db635c90a162c266
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-05-27 19:29:01 -0500

    separate PEN

---
+++ ml/SepPEN1.m
@@ -0,0 +1,14 @@
+function [sepPEN] = SepPEN1(PEN, num)
+% separate PEN to n=num sets
+l= size(PEN,1);
+
+s= floor(l/num);
+sepPEN=cell(1,num);
+for i=1:num-1
+    sepPEN{i} = PEN(1+(i-1)*s:i*s,:);
+    
+end
+sepPEN{num}=PEN(s*(num-1)+1:end,:);
+
+end
+
