e6194b0403dd96215adaccfb72b0ad277ee64fa3
Andreas Mang <andreas@math.uh.edu>
2021-05-28 14:25:00 -0500

    comments added

--- ml/exec.m
+++ ml/exec.m
@@ -1,26 +1,35 @@
-function exec( nodeid, numnodes )
- 
-       
-        g=5;
-        opt=setup(g);
-        F=opt.F;
-        Q=opt.Q;
-        m=opt.m;
-        M=m*Q;
+function runGreedySearchNID( nodeID, numnodes )
+% RUNGREEDYSEARCHNID runs the greedy search on a single node and stores
+% result in mat-file
+%
+% input:
+%   numodes     total number of nodes considered
+%   nodeID      id of particular node
 
-        H=[0.8 0.05 0.05 0.05 0.05];
-        G=[0.1 0.2 0.2 0.4 0.1];
-        mesh=0.005;
-        PEN=[0.05 0.2;0.1 0.3;0.1 0.3;0.3 0.4];
-        PENSET=PENset(PEN,mesh);
-        sepPEN = SepPEN1( PENSET, numnodes );     
-        
+% setup for problem
+g=5;
+opt=setup(g);
+F=opt.F;
+Q=opt.Q;
+m=opt.m;
+M=m*Q;
 
+H=[0.8 0.05 0.05 0.05 0.05];
+G=[0.1 0.2 0.2 0.4 0.1];
+mesh=0.005;
+PEN=[0.05 0.2;0.1 0.3;0.1 0.3;0.3 0.4];
 
-        [BP, cost_BP] = ParallelOneComp(sepPEN{nodeid}, 10, H, G, F, Q, m );
+%  construct the subsets of the search grid for the greedy algorithm
+PENSET=PENset(PEN,mesh);
+sepPEN = SepPEN1( PENSET, numnodes );
 
-        resultname = ['result-for-node', num2str(nodeid) '.mat' ];
+% execute greedy search on one node (with 10 parallel procs)
+[BP, cost_BP] = ParallelOneComp(sepPEN{nodeID}, 10, H, G, F, Q, m );
 
-        save( resultname, 'BP', 'cost_BP' )
+% construct file name for output
+resultname = ['result-for-node', num2str(nodeID) '.mat' ];
+
+% save to file
+save( resultname, 'BP', 'cost_BP' )
 
 end
