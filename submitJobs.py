33f4fd869d4a5aa6dac4b844ffa49bc336e9f5ec
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-05-31 13:17:54 -0500

    Update submitJobs.py

--- submitJobs.py
+++ submitJobs.py
@@ -27,7 +27,7 @@ opt.setdefault('code_directory', bdir);
 #    print("results already computed: ", wdir)
 #    quit(); # if already computed, go home
 
-numnodes = 20;
+numnodes = 10;
 
 for nodeID in range(1, numnodes+1):
 
