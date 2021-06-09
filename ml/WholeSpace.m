173fb9db0fb466ba673eee3c45351f508e92d288
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-06-09 12:56:12 -0500

    Add files via upload

---
+++ ml/WholeSpace.m
@@ -0,0 +1,22 @@
+function [Whole] = WholeSpace(H,mesh)
+% this function returns all the penultimate histograms by using mesh to
+% discretize PEN.
+g=size(H,2);
+sets=cell(1,g-1);
+Leng=1;
+
+for i=1:g-1
+    sets{i}=0.99:-mesh:0.01;
+    Leng=Leng* size(sets{i},2);
+    
+end
+PENSET=zeros(Leng,g);
+c = cell(1, numel(sets));
+[c{:}] = ndgrid( sets{:} );
+result = cell2mat( cellfun(@(v)v(:), c, 'UniformOutput',false) );
+rrr=result(find(sum(result,2)<0.999),:);
+y=1-sum(rrr,2);
+Whole= [rrr y];
+
+
+end
