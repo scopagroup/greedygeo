a57d448da5b141089e50ed8ecb3c5c4f9f1d3c43
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-06-12 17:40:07 -0500

    Update GeneratePen.m

--- ml/GeneratePen.m
+++ ml/GeneratePen.m
@@ -3,7 +3,7 @@ function [PENset] = GeneratePen(I, G, F, m, Q, Whole, c)
 if I==2
    PENset=[]; 
 for i=1:size(Whole,1)
-    if OneStepCost(Whole(i,:),G,F,m,Q)<=c
+    if OneStepCost(Whole(i,:),G,F,m,Q)<=c && OneStepCost(Whole(i,:),G,F,m,Q)>0
         PENset=[PENset; Whole(i,:)];
     end
 end
