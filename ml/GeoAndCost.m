2b7b76377698a153475df78d46a1b7386f1c58f9
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-06-12 12:50:53 -0500

    Add files via upload

--- ml/GeoAndCost.m
+++ ml/GeoAndCost.m
@@ -1,20 +1,29 @@
-function [GEOSET,RevGEO, COSTSET] = GeoAndCost(H, TAR, F, m, Q, mesh)
+function [GEOSET,RevGEO, COSTSET, HG, DistAndCost] = GeoAndCost( HGpair, F, m, Q, mesh, Compare)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
-n=size(TAR,1);
+
+n=size(HGpair,1);
+g=size(HGpair,2)/2;
 GEOSET=cell(1,n);
 RevGEO=cell(1,n);
 COSTSET=cell(1,n);
-for i=1:size(TAR,1)
-    G=TAR(i,:);
+HG=[];
+DistAndCost=[];
+I=1;
+for i=1:size(HGpair,1)
+    H=HGpair(i,1:g);
+    G=HGpair(i,g+1:2*g);
+    CCC=Compare(i,I);
     [Tra1, cost_store1, comp] = PreTest(H,G,F,m,Q);
     Whole=WholeSpace(G,mesh);
-    [PENset] = GeneratePen(G, F, m, Q, Whole, comp);
+    [PENset] = GeneratePen(I, G, F, m, Q, Whole, CCC);
     [GEO,COST,GEO1,COST1,Total_cost,Count] = GeodesicAndCost2(H,G,PENset,F,Q,m,comp);
     [a b]=min(Total_cost(1:Count-1));
     RevGEO{i}=GEO{b};
     GEOSET{i}= GEO1{b};
     COSTSET{i}= COST1{b};
+    HG=[HG;H G];
+    DistAndCost=[DistAndCost; norm(GEO1{b}(1,:)-GEO1{b}(2,:),Inf) COST1{b}(2)];
 end
     
 end
