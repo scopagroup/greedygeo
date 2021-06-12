dd2c03abb235d268fd072f3a1dbbd94a9a59c7f0
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-06-12 12:51:27 -0500

    Add files via upload

--- ml/GeneratePen.m
+++ ml/GeneratePen.m
@@ -1,10 +1,16 @@
-function [PENset] = GeneratePen(G, F, m, Q, Whole, c)
-PENset=[];
+function [PENset] = GeneratePen(I, G, F, m, Q, Whole, c)
+
+if I==2
+   PENset=[]; 
 for i=1:size(Whole,1)
-    if OneStepCost(Whole(i,:),G,F,m,Q)<c
+    if OneStepCost(Whole(i,:),G,F,m,Q)<=c
         PENset=[PENset; Whole(i,:)];
     end
 end
+else
+    PENset=Whole(vecnorm(Whole-G, Inf, 2)<=c,:);
+end
+    
 
 
 end
