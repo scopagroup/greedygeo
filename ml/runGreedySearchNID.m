8ef2cda64ce8fc28001d5a4bdb1c8a7a2be0e21e
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-07-08 17:27:38 -0500

    Update runGreedySearchNID.m

--- ml/runGreedySearchNID.m
+++ ml/runGreedySearchNID.m
@@ -8,18 +8,18 @@ function runGreedySearchNID( nodeID, numnodes, outdir )
 
 % setup for problem
 
-g=5;
+g=7;
 opt=setup(g);
 F=opt.F;
 Q=opt.Q;
 m=opt.m;
 M=m*Q;
-H=[0.8 0.05 0.05 0.05 0.05];
-G=[0.1 0.1 0.2 0.5 0.1];
-%H=[0.6, 0.1,0.1 0.05, 0.05, 0.05, 0.05];
-%G=[0.1, 0.1, 0.1, 0.1,0.1 , 0.5, 0.1];
+%H=[0.8 0.05 0.05 0.05 0.05];
+%G=[0.1 0.1 0.2 0.5 0.1];
+H=[0.6, 0.1,0.1, 0.05, 0.05, 0.05, 0.05];
+G=[0.1, 0.1, 0.1, 0.1,0.1 , 0.5, 0.1];
 mesh=0.01;
-PEN=[0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9;];%0.1 0.9;0.1 0.9];
+PEN=[0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9];
 p= 0.1;
 [quan] = Quan(G,g,F,m,Q,p);
 
@@ -32,8 +32,9 @@ PENSET1= SepPEN(PEN, numnodes);
 
 fprintf('executing search on node %d\n', nodeID);
 [BP, cost_BP,Count] = ParallelOneComp(PENSET1{nodeID}, 10, H, G, F, Q, m, mesh, quan );
+time=toc;
 % construct file name for output
-resultname = [outdir,'/','dim5quan0.1result-for-node', num2str(nodeID) '.mat' ];
+resultname = [outdir,'/','dim7quan0.1result-for-node', num2str(nodeID) '.mat' ];
 
 % save to file
 save( resultname, 'BP', 'cost_BP', 'Count', 'time','H','G')
