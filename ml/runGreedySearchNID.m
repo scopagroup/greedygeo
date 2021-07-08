c1713bc1d5bf24a290f9b3f8f28994e4565768d5
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-07-07 20:37:38 -0500

    Update runGreedySearchNID.m

--- ml/runGreedySearchNID.m
+++ ml/runGreedySearchNID.m
@@ -7,6 +7,7 @@ function runGreedySearchNID( nodeID, numnodes, outdir )
 %   nodeID      id of particular node
 
 % setup for problem
+
 g=5;
 opt=setup(g);
 F=opt.F;
@@ -15,31 +16,26 @@ m=opt.m;
 M=m*Q;
 H=[0.8, 0.05, 0.05, 0.05, 0.05];
 G=[0.1, 0.1, 0.2, 0.5, 0.1];
-mesh=0.002;
-PEN=[0.1 0.2;0.1 0.12;0.15 0.25;0.42 0.46];
+mesh=0.01;
+PEN=[0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9];
+p= 0.25;
+[quan] = Quan(G,g,F,m,Q,p);
 
-%H=[0.8 0.05 0.05 0.05 0.05];
-%G=[0.1 0.2 0.2 0.4 0.1];
-%mesh=0.005;
-%PEN=[0.05 0.2;0.1 0.3;0.1 0.3;0.3 0.4];
 
 %  construct the subsets of the search grid for the greedy algorithm
 tic
-PENSET=PENset(PEN,mesh);
-sepPEN = SepPEN1( PENSET, numnodes );
-C = size(sepPEN{nodeID},1);
+PENSET1= SepPEN(PEN, numnodes);
+
 % execute greedy search on one node (with 10 parallel procs)
 
 fprintf('executing search on node %d\n', nodeID);
-[BP, cost_BP,Count] = ParallelOneComp(sepPEN{nodeID}, 10, H, G, F, Q, m );
+[BP, cost_BP,Count] = ParallelOneComp(PENSET1{nodeID}, 6, H, G, F, Q, m, mesh, quan );
 time=toc
 % construct file name for output
-resultname = [outdir,'/','result-for-node', num2str(nodeID) '.mat' ];
+resultname = [outdir,/,'result-for-node', num2str(nodeID) '.mat' ];
 
 % save to file
-save( resultname, 'BP', 'cost_BP', 'C', 'Count', 'time')
-
-
+save( resultname, 'BP', 'cost_BP', 'Count', 'time')
 
 
 
