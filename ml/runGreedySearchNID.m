6f2768e8f83ddc5fe8378020ef680d8b1ab36e41
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-07-08 22:48:54 -0500

    Update runGreedySearchNID.m

--- ml/runGreedySearchNID.m
+++ ml/runGreedySearchNID.m
@@ -18,7 +18,7 @@ M=m*Q;
 %G=[0.1 0.1 0.2 0.5 0.1];
 H=[0.5, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05];
 G=[0.08, 0.08, 0.08, 0.08,0.08 , 0.08,0.44, 0.08];
-mesh=0.05;
+mesh=0.02;
 PEN=[0.05 0.8;0.05 0.8;0.05 0.8;0.05 0.8;0.05 0.8;0.05 0.8;0.2 0.6];
 p= 0.1;
 [quan] = Quan(G,g,F,m,Q,p);
@@ -34,7 +34,7 @@ fprintf('executing search on node %d\n', nodeID);
 [BP, cost_BP,Count] = ParallelOneComp(PENSET1{nodeID}, 16, H, G, F, Q, m, mesh, quan );
 time=toc;
 % construct file name for output
-resultname = [outdir,'/','dim8quan0.1result-for-node', num2str(nodeID) '.mat' ];
+resultname = [outdir,'/','dim8quan0.1result1-for-node', num2str(nodeID) '.mat' ];
 
 % save to file
 save( resultname, 'BP', 'cost_BP', 'Count', 'time','H','G','mesh')
