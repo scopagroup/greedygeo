2a266812871f72e44ce0b3b67d1a20824729301f
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-07-21 12:52:30 -0500

    Update runGreedySearchNID.m

--- ml/runGreedySearchNID.m
+++ ml/runGreedySearchNID.m
@@ -22,7 +22,7 @@ H=[0.5, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05];
 G=[0.05, 0.05, 0.1, 0.05,0.05 , 0.1,0.5, 0.1];
 %H=[0.5, 0.1 0.1 0.1 0.1 0.1];
 %G=[0.1 0.1 0.1 0.1 0.5 0.1];
-mesh=0.05;
+mesh=0.03;
 %PEN=[0.005 0.5;0.005 0.5;0.005 0.5;0.005 0.5;0.005 0.5;0.005 0.5;0.25 0.6];
 %PEN=[0.005 0.25;0.005 0.25 ;0.005 0.25 ;0.005 0.25; 0.005 0.25; 0.005 0.5;0.25 0.6];
 PEN=[0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.1 0.9 ];
@@ -40,7 +40,7 @@ fprintf('executing search on node %d\n', nodeID);
 [BP, cost_BP,Count] = ParallelOneComp(PENSET1{nodeID}, 16, H, G, F, Q, m, mesh, quan );
 time=toc;
 % construct file name for output
-resultname = [outdir,'/','dim8quan0.05result1-mesh0.05-for-node', num2str(nodeID) '.mat' ];
+resultname = [outdir,'/','dim8quan0.05result1-mesh0.03-for-node', num2str(nodeID) '.mat' ];
 
 % save to file
 save( resultname, 'BP', 'cost_BP', 'Count', 'time','H','G','mesh','PEN')
