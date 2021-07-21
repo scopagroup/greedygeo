0b6d0e8a948f47af8b5a096ebb3fe7922dd1decd
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-07-21 17:33:00 -0500

    Update runGreedySearchNID.m

--- ml/runGreedySearchNID.m
+++ ml/runGreedySearchNID.m
@@ -8,26 +8,26 @@ function runGreedySearchNID( nodeID, numnodes, outdir )
 
 % setup for problem
 
-g=6;
+g=7;
 opt=setup(g);
 F=opt.F;
 Q=opt.Q;
 m=opt.m;
 M=m*Q;
-%H=[0.6, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05]
-%G=[0.1, 0.1, 0.1, 0.1, 0.1, 0.4, 0.1] 
+H=[0.6, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05]
+G=[0.1, 0.1, 0.1, 0.1, 0.1, 0.4, 0.1] 
 %H=[0.8 0.05 0.05 0.05 0.05];
 %G=[0.1 0.1 0.2 0.5 0.1];
 %H=[0.5, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05];
 %G=[0.05, 0.05, 0.1, 0.05,0.05 , 0.1,0.5, 0.1];
-H=[0.5, 0.1 0.1 0.1 0.1 0.1];
-G=[0.1 0.1 0.1 0.1 0.5 0.1];
-mesh=0.01;
+%H=[0.5, 0.1 0.1 0.1 0.1 0.1];
+%G=[0.1 0.1 0.1 0.1 0.5 0.1];
+mesh=0.02;
 %PEN=[0.005 0.5;0.005 0.5;0.005 0.5;0.005 0.5;0.005 0.5;0.005 0.5;0.25 0.6];
 %PEN=[0.005 0.25;0.005 0.25 ;0.005 0.25 ;0.005 0.25; 0.005 0.25; 0.005 0.5;0.25 0.6];
-%PEN=[0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.1 0.9 ];
-PEN=[0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9];
-p= 0.2;
+PEN=[0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9;0.005 0.9 ];
+%PEN=[0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9;0.1 0.9];
+p= 0.1;
 [quan] = Quan(G,g,F,m,Q,p);
 
 
@@ -41,7 +41,7 @@ fprintf('executing search on node %d\n', nodeID);
 [BP, cost_BP,Count] = ParallelOneComp(PENSET1{nodeID}, 8, H, G, F, Q, m, mesh, quan );
 time=toc;
 % construct file name for output
-resultname = [outdir,'/','dim6quan0.2result1-mesh0.01-for-node', num2str(nodeID) '.mat' ];
+resultname = [outdir,'/','dim7quan0.1result1-mesh0.02-for-node', num2str(nodeID) '.mat' ];
 
 % save to file
 save( resultname, 'BP', 'cost_BP', 'Count', 'time','H','G','mesh','PEN')
