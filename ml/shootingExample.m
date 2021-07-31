a2a86ea6e6a82b6a88b5917858ac19eee739322f
yingxuesu <68074656+yingxuesu@users.noreply.github.com>
2021-07-31 17:08:52 -0500

    Update shootingExample.m

--- ml/shootingExample.m
+++ ml/shootingExample.m
@@ -6,9 +6,9 @@ Q=opt.Q;
 m=opt.m;
 M=m*Q;
 H=[0.7 0.1 0.1 0.1];
-G=[0.2 0.5 0.2 0.1];
+G=[0.1 0.1 0.7 0.1];
 eps=10^(-4);
-para=10^(-4);
+para=10^(-5);
 tic
 [FGEO] = Shooting(H,G,F,m,Q,eps,para);
 toc
